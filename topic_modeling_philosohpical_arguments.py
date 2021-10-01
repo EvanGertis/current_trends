f2ca23e1cf88e30a8297ab9a755f261168203ac7
EvanGertis <physevangertis@gmail.com>
2021-10-01 06:28:52 -0400

    restructure codebase

--- topic_modeling_philosohpical_arguments.py
+++ topic_modeling_philosohpical_arguments.py
@@ -1,81 +1,52 @@
-# Importing modules
 import pandas as pd
 import os
+import re
+import gensim
+import numpy as np
+import tqdm
+import pyLDAvis.gensim_models
+import pickle 
+import pyLDAvis
+import nltk
+import spacy
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from nltk.corpus import stopwords
+from pprint import pprint
+from gensim.models import CoherenceModel
+
+
 
 def main():
 
-    # os.chdir('..')
-
-    # Read data into papers
-    papers = pd.read_csv('philosophy.csv')
-
-    # Print head
-    print(papers.head())
-
-    papers
-
-    # Remove the columns
-    papers = papers.drop(columns=['created_utc','score','domain','id','link_flair_text','over_18','thumbnail','subreddit_id','edited','link_flair_css_class','author_flair_css_class','is_self','name','url','distinguished','ups','downs','num_comments','permalink'], axis=1)
+    reddit_posts = pd.read_csv('philosophy.csv')
+    reddit_posts = reddit_posts.drop(columns=['created_utc','score','domain','id','link_flair_text','over_18','thumbnail','subreddit_id','edited','link_flair_css_class','author_flair_css_class','is_self','name','url','distinguished','ups','downs','num_comments','permalink'], axis=1)
     
-    papers = papers.dropna()
-    print(papers)
-
-    # sample only 100 papers
-    papers = papers.sample(100)
-    print(papers)
-
-    # Print out the first rows of papers
-    papers.head()
-
-    print(len(papers.index))
-
-    # Load the regular expression library
-    import re
-
-    # Remove punctuation
-    papers['paper_text_processed'] = papers['selftext'].map(lambda x: re.sub('[,\.!?]', '', x))
-
-    print(papers['paper_text_processed'])
-
-    # Convert the titles to lowercase
-    papers['paper_text_processed'] = papers['paper_text_processed'].map(lambda x: x.lower())
-
-    print(papers['paper_text_processed'])
-
-    # Print out the first rows of papers
-    papers['paper_text_processed'].head()
-
-    print(papers['paper_text_processed'].head())
-
-    import gensim
-    from gensim.utils import simple_preprocess
+    reddit_posts = reddit_posts.dropna()
+    reddit_posts = reddit_posts.sample(100)
+    reddit_posts.head()
+    reddit_posts['reddit_posts_text_processed'] = reddit_posts['selftext'].map(lambda x: re.sub('[,\.!?]', '', x))
+    reddit_posts['reddit_posts_text_processed']
+    reddit_posts['reddit_posts_text_processed'] = reddit_posts['reddit_posts_text_processed'].map(lambda x: x.lower())
+    reddit_posts['reddit_posts_text_processed']
+    reddit_posts['reddit_posts_text_processed'].head()
+    reddit_posts['reddit_posts_text_processed'].head()
 
     def sent_to_words(sentences):
         for sentence in sentences:
             yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
-
-    data = papers.paper_text_processed.values.tolist()
+    data = reddit_posts.paper_text_processed.values.tolist()
     data_words = list(sent_to_words(data))
-
-    print(data_words[:1][0][:30])
-
-    # Build the bigram and trigram models
+    data_words[:1][0][:30]
     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
     trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
-
-    # Faster way to get a sentence clubbed as a trigram/bigram
     bigram_mod = gensim.models.phrases.Phraser(bigram)
     trigram_mod = gensim.models.phrases.Phraser(trigram)
-
-    # NLTK Stop words
-    import nltk
     nltk.download('stopwords')
-    from nltk.corpus import stopwords
 
     stop_words = stopwords.words('english')
     stop_words.extend(["the","of","to","and","a","in","is","it","you","that","he","was","for","on","are","with","as","I","his","they","be","at","one","have","this","from","or","had","by","not","word","but","what","some","we","can","out","other","were","all","there","when","up","use","your","how","said","an","each","she","which","do","their","time","if","will","way","about","many","then","them","write","would","like","so","these","her","long","make","thing","see","him","two","has","look","more","day","could","go","come","did","number","sound","no","most","people","my","over","know","water","than","call","first","who","may","down","side","been","now","find","any","new","work","part","take","get","place","made","live","where","after","back","little","only","round","man","year","came","show","every","good","me","give","our","under","name","very","through","just","form","sentence","great","think","say","help","low","line","differ","turn","cause","much","mean","before","move","right","boy","old","too","same","tell","does","set","three","want","air","well","also","play","small","end","put","home","read","hand","port","large","spell","add","even","land","here","must","big","high","such","follow","act","why","ask","men","change","went","light","kind","off","need","house","picture","try","us","again","animal","point","mother","world","near","build","self","earth","father","head","stand","own","page","should","country","found","answer","school","grow","study","still","learn","plant","cover","food","sun","four","between","state","keep","eye","never","last","let","thought","city","tree","cross","farm","hard","start","might","story","saw","far","sea","draw","left","late","run","don't","while","press","close","night","real","life","few","north","open","seem","together","next","white","children","begin","got","walk","example","ease","paper","group","always","music","those","both","mark","often","letter","until","mile","river","car","feet","care","second","book","carry","took","science","eat","room","friend","began","idea","fish","mountain","stop","once","base","hear","horse","cut","sure","watch","color","face","wood","main","enough","plain","girl","usual","young","ready","above","ever","red","list","though","feel","talk","bird","soon","body","dog","family","direct","pose","leave","song","measure","door","product","black","short","numeral","class","wind","question","happen","complete","ship","area","half","rock","order","fire","south","problem","piece","told","knew","pass","since","top","whole","king","space","heard","best","hour","better","true","during","hundred","five","remember","step","early","hold","west","ground","interest","reach","fast","verb","sing","listen","six","table","travel","less","morning","ten","simple","several","vowel","toward","war","lay","against","pattern","slow","center","love","person","money","serve","appear","road","map","rain","rule","govern","pull","cold","notice","voice","unit","power","town","fine","certain","fly","fall","lead","cry","dark","machine","note","wait","plan","figure","star","box","noun","field","rest","correct","able","pound","done","beauty","drive","stood","contain","front","teach","week","final","gave","green","oh","quick","develop","ocean","warm","free","minute","strong","special","mind","behind","clear","tail","produce","fact","street","inch","multiply","nothing","course","stay","wheel","full","force","blue","object","decide","surface","deep","moon","island","foot","system","busy","test","record","boat","common","gold","possible","plane","stead","dry","wonder","laugh","thousand","ago","ran","check","game","shape","equate","hot","miss","brought","heat","snow","tire","bring","yes","distant","fill","east","paint","language","among","grand","ball","yet","wave","drop","heart","am","present","heavy","dance","engine","position","arm","wide","sail","material","size","vary","settle","speak","weight","general","ice","matter","circle","pair","include","divide","syllable","felt","perhaps","pick","sudden","count","square","reason","length","represent","art","subject","region","energy","hunt","probable","bed","brother","egg","ride","cell","believe","fraction","forest","sit","race","window","store","summer","train","sleep","prove","lone","leg","exercise","wall","catch","mount","wish","sky","board","joy","winter","sat","written","wild","instrument","kept","glass","grass","cow","job","edge","sign","visit","past","soft","fun","bright","gas","weather","month","million","bear","finish","happy","hope","flower","clothe","strange","gone","jump","baby","eight","village","meet","root","buy","raise","solve","metal","whether","push","seven","paragraph","third","shall","held","hair","describe","cook","floor","either","result","burn","hill","safe","cat","century","consider","type","law","bit","coast","copy","phrase","silent","tall","sand","soil","roll","temperature","finger","industry","value","fight","lie","beat","excite","natural","view","sense","ear","else","quite","broke","case","middle","kill","son","lake","moment","scale","loud","spring","observe","child","straight","consonant","nation","dictionary","milk","speed","method","organ","pay","age","section","dress","cloud","surprise","quiet","stone","tiny","climb","cool","design","poor","lot","experiment","bottom","key","iron","single","stick","flat","twenty","skin","smile","crease","hole","trade","melody","trip","office","receive","row","mouth","exact","symbol","die","least","trouble","shout","except","wrote","seed","tone","join","suggest","clean","break","lady","yard","rise","bad","blow","oil","blood","touch","grew","cent","mix","team","wire","cost","lost","brown","wear","garden","equal","sent","choose","fell","fit","flow","fair","bank","collect","save","control","decimal","gentle","woman","captain","practice","separate","difficult","doctor","please","protect","noon","whose","locate","ring","character","insect","caught","period","indicate","radio","spoke","atom","human","history","effect","electric","expect","crop","modern","element","hit","student","corner","party","supply","bone","rail","imagine","provide","agree","thus","capital","won't","chair","danger","fruit","rich","thick","soldier","process","operate","guess","necessary","sharp","wing","create","neighbor","wash","bat","rather","crowd","corn","compare","poem","string","bell","depend","meat","rub","tube","famous","dollar","stream","fear","sight","thin","triangle","planet","hurry","chief","colony","clock","mine","tie","enter","major","fresh","search","send","yellow","gun","allow","print","dead","spot","desert","suit","current","lift","rose","continue","block","chart","hat","sell","success","company","subtract","event","particular","deal","swim","term","opposite","wife","shoe","shoulder","spread","arrange","camp","invent","cotton","born","determine","quart","nine","truck","noise","level","chance","gather","shop","stretch","throw","shine","property","column","molecule","select","wrong","gray","repeat","require","broad","prepare","salt","nose","plural","anger","claim","continent","oxygen","sugar","death","pretty","skill","women","season","solution","magnet","silver","thank","branch","match","suffix","especially","fig","afraid","huge","sister","steel","discuss","forward","similar","guide","experience","score","apple","bought","led","pitch","coat","mass","card","band","rope","slip","win","dream","evening","condition","feed","tool","total","basic","smell","valley","nor","double","seat","arrive","master","track","parent","shore","division","sheet","substance","favor","connect","post","spend","chord","fat","glad","original","share","station","dad","bread","charge","proper","bar","offer","segment","slave","duck","instant","market","degree","populate","chick","dear","enemy","reply","drink","occur","support","speech","nature","range","steam","motion","path","liquid","log","meant","quotient","teeth","shell","neck","edit"])
 
-    # Define functions for stopwords, bigrams, trigrams and lemmatization
     def remove_stopwords(texts):
         return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
@@ -93,38 +64,15 @@ def main():
             texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
         return texts_out
 
-    import spacy
-
-    # Remove Stop Words
     data_words_nostops = remove_stopwords(data_words)
-
-    # Form Bigrams
     data_words_bigrams = make_bigrams(data_words_nostops)
-
-    # Initialize spacy 'en' model, keeping only tagger component (for efficiency)
     nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
-
-    # Do lemmatization keeping only noun, adj, vb, adv
     data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
-
-    print(data_lemmatized[:1][0][:30])
-
-    import gensim.corpora as corpora
-
-    # Create Dictionary
+    data_lemmatized[:1][0][:30]
     id2word = corpora.Dictionary(data_lemmatized)
-
-    # Create Corpus
     texts = data_lemmatized
-
-    # Term Document Frequency
     corpus = [id2word.doc2bow(text) for text in texts]
-
-    # View
-    print(corpus[:1][0][:30])
-
-
-    # Build LDA model
+    corpus[:1][0][:30]
     lda_model = gensim.models.LdaMulticore(corpus=corpus,
                                         id2word=id2word,
                                         num_topics=10, 
@@ -133,15 +81,8 @@ def main():
                                         passes=10,
                                         per_word_topics=True)
 
-    from pprint import pprint
-
-    # Print the Keyword in the 10 topics
     pprint(lda_model.print_topics())
     doc_lda = lda_model[corpus]
-
-    from gensim.models import CoherenceModel
-
-    # Compute Coherence Score
     coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
     coherence_lda = coherence_model_lda.get_coherence()
     print('Coherence Score: ', coherence_lda)
@@ -162,58 +103,35 @@ def main():
         coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
         
         return coherence_model_lda.get_coherence()
-
-    import numpy as np
-    import tqdm
-
     grid = {}
     grid['Validation_Set'] = {}
-
-    # Topics range
     min_topics = 2
     max_topics = 11
     step_size = 1
     topics_range = range(min_topics, max_topics, step_size)
-
-    # Alpha parameter
     alpha = list(np.arange(0.01, 1, 0.3))
     alpha.append('symmetric')
     alpha.append('asymmetric')
-
-    # Beta parameter
     beta = list(np.arange(0.01, 1, 0.3))
     beta.append('symmetric')
-
-    # Validation sets
     num_of_docs = len(corpus)
     corpus_sets = [gensim.utils.ClippedCorpus(corpus, int(num_of_docs*0.75)), 
                    corpus]
-
     corpus_title = ['75% Corpus', '100% Corpus']
-
     model_results = {'Validation_Set': [],
                      'Topics': [],
                      'Alpha': [],
                      'Beta': [],
                      'Coherence': []
                     }
-
-    # Can take a long time to run
     if 1 == 1:
         pbar = tqdm.tqdm(total=(len(beta)*len(alpha)*len(topics_range)*len(corpus_title)))
-        
-        # iterate through validation corpuses
         for i in range(len(corpus_sets)):
-            # iterate through number of topics
             for k in topics_range:
-                # iterate through alpha values
                 for a in alpha:
-                    # iterare through beta values
                     for b in beta:
-                        # get the coherence score for the given parameters
                         cv = compute_coherence_values(corpus=corpus_sets[i], dictionary=id2word, 
                                                       k=k, a=a, b=b)
-                        # Save the model results
                         model_results['Validation_Set'].append(corpus_title[i])
                         model_results['Topics'].append(k)
                         model_results['Alpha'].append(a)
@@ -234,25 +152,10 @@ def main():
                                                passes=10,
                                                alpha=0.01,
                                                eta=0.9)
-
-    from pprint import pprint
-
-    # Print the Keyword in the 10 topics
     pprint(lda_model.print_topics())
     doc_lda = lda_model[corpus]
-
-
-    import pyLDAvis.gensim_models
-    import pickle 
-    import pyLDAvis
-
     # Visualize the topics
-    # pyLDAvis.enable_notebook()
-
     LDAvis_data_filepath = os.path.join('./results/ldavis_tuned_'+str(num_topics))
-
-    # # this is a bit time consuming - make the if statement True
-    # # if you want to execute visualization prep yourself
     if 1 == 1:
         LDAvis_prepared = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word)
         with open(LDAvis_data_filepath, 'wb') as f:
